40eaf2dd1f10b41ba71b1b327e398c0c8b173502
vishalicious213 <vishalicious213@gmail.com>
2019-10-11 20:52:58 -0400

    tabs - got data from lambda with axios (arrays of objects)

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,12 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(response => {
+        // console.log("Got data from Lambda", response);
+        console.log(response.data.articles);
+    })
+    .catch(err => {
+        console.log("Got error from Lambda", err);
+    })
