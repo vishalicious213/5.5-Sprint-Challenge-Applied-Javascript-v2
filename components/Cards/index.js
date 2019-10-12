c44867474100ccd877090a41b089a08fe5f31364
vishalicious213 <vishalicious213@gmail.com>
2019-10-11 23:57:14 -0400

    cards - got the right data

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -29,19 +29,53 @@ axios.get("https://lambda-times-backend.herokuapp.com/articles")
         // console.log(Object.entries(response.data.articles));
 
         let info = Object.entries(response.data.articles);
-        /*
-        console.log("1", info);          // all categories and articles
-        console.log("2", info[0]);       // 1 category (key) and its articles (values)
-        console.log("3", info[0][0]);    // 1 category name (key & value 0)
-        console.log("4", info[0][1]);    // all articles from 1 category (values)
-        console.log("5", info[0][1][0]); // 1 article from 1 category
-        console.log("6", info[0][1][0]["authorName"]);
-        console.log("7", info[0][1][0]["authorPhoto"]);
-        console.log("8", info[0][1][0]["headline"]);
-        */
+
+        // console.log("1", info);          // all categories and articles
+        // console.log("2", info[0]);       // 1 category (key) and its articles (values)
+        // console.log("3", info[0][0]);    // 1 category name (key & value 0)
+        // console.log("4", info[0][1]);    // all articles from 1 category (values)
+        // console.log("5", info[0][1][0]); // 1 article from 1 category
+        // console.log("6", info[0][1][0]["authorName"]);
+        // console.log("7", info[0][1][0]["authorPhoto"]);
+        // console.log("8", info[0][1][0]["headline"]);
 
         // pseudo: for each category, for each article, get name/photo/headline
-        // categories: info[0][0]
+        // categories: info[category][category(0) article(1)][article]
+
+        let numCategories = info.length; // number of categories
+        // console.log("Number of categories ", numCategories);
+
+        let categoryCount;
+        for (categoryCount = 0; categoryCount < numCategories; categoryCount++) {
+            // console.log(info[categoryCount][0]);
+            let category = (info[categoryCount][0]);
+            // console.log("Category: ", category);
+            
+            // console.log(info[categoryCount][1].length); // number of articles per category
+            let numArticles = (info[categoryCount][1].length);
+            // console.log("Number of articles", numArticles);
+
+            let articleCount;
+            for (articleCount = 0; articleCount < numArticles; articleCount++) {
+                // console.log(info[0][categoryCount][articleCount])
+                // console.log(info[categoryCount][1][articleCount]["authorName"])
+                
+                let article = (info[categoryCount][1][articleCount]);
+                // console.log(article["authorName"]);
+                // console.log(article["authorPhoto"]);
+                // console.log(article["headline"]);
+
+                const authorName = (article["authorName"]);
+                const authorPhoto = (article["authorPhoto"]);
+                const headline = (article["headline"]);
+                // console.log(authorName);
+                // console.log(authorPhoto);
+                // console.log(headline);
+
+            } // for articleCount
+            
+            
+        } // for categoryCount
 
         
     })
