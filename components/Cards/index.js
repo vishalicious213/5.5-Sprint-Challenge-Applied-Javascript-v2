694d849e2c8ebe654c6e7ff05e915afaa9ed6291
vishalicious213 <vishalicious213@gmail.com>
2019-10-11 22:05:57 -0400

    cards - figuring out how to grab the right data

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -21,7 +21,29 @@
 axios.get("https://lambda-times-backend.herokuapp.com/articles")
     .then(response => {
         // console.log("Got data from Lambda", response);
-        console.log(response.data.articles);
+        // console.log(response.data.articles);
+
+        // const category = response.data.articles;
+        // console.log(category);
+
+        // console.log(Object.entries(response.data.articles));
+
+        let info = Object.entries(response.data.articles);
+        /*
+        console.log("1", info);          // all categories and articles
+        console.log("2", info[0]);       // 1 category (key) and its articles (values)
+        console.log("3", info[0][0]);    // 1 category name (key & value 0)
+        console.log("4", info[0][1]);    // all articles from 1 category (values)
+        console.log("5", info[0][1][0]); // 1 article from 1 category
+        console.log("6", info[0][1][0]["authorName"]);
+        console.log("7", info[0][1][0]["authorPhoto"]);
+        console.log("8", info[0][1][0]["headline"]);
+        */
+
+        // pseudo: for each category, for each article, get name/photo/headline
+        // categories: info[0][0]
+
+        
     })
     .catch(err => {
         console.log("Got error from Lambda", err);
